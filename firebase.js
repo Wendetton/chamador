986b61e835348fd6389d8ec7be1efbdaa5f25a90
Wendetton <feazegoncalves@gmail.com>
2025-06-27 20:34:21 -0300

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCjkC_k4xmbY6VvPwN4-gVPuGgMRgQyQCQ',
-  authDomain: 'chamador-c46ab.firebaseapp.com',
-  projectId: 'chamador-c46ab',
-  storageBucket: 'chamador-c46ab.firebasestorage.app',
-  messagingSenderId: '44597594571',
-  appId: '1:44597594571:web:a9f57c9ece2cbbcec58e4e',
+  apiKey: 'SUA_API_KEY',
+  authDomain: 'SEU_DOMINIO.firebaseapp.com',
+  projectId: 'SEU_PROJECT_ID',
+  storageBucket: 'SEU_BUCKET.appspot.com',
+  messagingSenderId: 'SEU_MESSAGING_ID',
+  appId: 'SEU_APP_ID',
 };
 
 const app = initializeApp(firebaseConfig);
